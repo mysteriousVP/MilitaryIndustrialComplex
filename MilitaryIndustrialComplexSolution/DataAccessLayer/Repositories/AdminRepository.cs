d78900be58c547842ab30561478fc9b7a3224e83
unknown <guniv12@gmail.com>
2020-06-04 10:01:13 +0300

    Added main repositories
    Added main repositories

---
+++ MilitaryIndustrialComplexSolution/DataAccessLayer/Repositories/AdminRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Context;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Repositories
+{
+    public class AdminRepository : BaseRepository<Admin>, IAdminRepository
+    {
+        internal AdminRepository(MilitaryComplexContex context)
+            : base(context)
+        {
+        }
+    }
+}
---
+++ MilitaryIndustrialComplexSolution/DataAccessLayer/Repositories/ProductionStoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Context;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Repositories
+{
+    public class ProductionStoreRepository : BaseRepository<ProductionStore>, IProductionStoreRepository
+    {
+        internal ProductionStoreRepository(MilitaryComplexContex context)
+            : base(context)
+        {
+        }
+    }
+}
