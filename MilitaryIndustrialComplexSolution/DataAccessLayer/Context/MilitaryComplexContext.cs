1201ca9d8f8146a69655704e0da8af8873a9abf2
unknown <guniv12@gmail.com>
2020-06-04 09:53:18 +0300

    Added database context
    Added database context

---
+++ MilitaryIndustrialComplexSolution/DataAccessLayer/Context/MilitaryComplexContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Context
+{
+    public class MilitaryComplexContex : DbContext
+    {
+        public DbSet<Admin> Phones { get; set; }
+        public DbSet<ProductionStore> Orders { get; set; }
+​
+        public MilitaryComplexContex(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
