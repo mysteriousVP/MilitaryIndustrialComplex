c530696946c4e5962ab6c8c67b683b5138df0242
unknown <guniv12@gmail.com>
2020-06-04 09:49:22 +0300

    Added Entities to DAL
    Added Entities to DAL

---
+++ MilitaryIndustrialComplexSolution/DataAccessLayer/Entities/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    public class Admin
+    {
+        public string Name { get; set; }
+        public float Quality { get; set; }
+        public string AdminLicense { get; set; }
+        public ProductionStore ProductionStore { get; set; }
+    }
+}
---
+++ MilitaryIndustrialComplexSolution/DataAccessLayer/Entities/ProductionStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    public class ProductionStore
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public IList<string> ProductList { get; set; }
+    }
+}
