7a97990f48d3a32db8dd4b80a289a572f5e4ec45
unknown <guniv12@gmail.com>
2020-06-04 10:39:11 +0300

    Added concrete TestProductionStoreRepository for testing
    Added concrete TestProductionStoreRepository for testing

---
+++ MilitaryIndustrialComplexSolution/DALTests/TestProductionStoreRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DALTests
+{
+    class TestProductionStoreRepository : BaseRepository<ProductionStore>
+    {
+        public TestProductionStoreRepository(DbContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
