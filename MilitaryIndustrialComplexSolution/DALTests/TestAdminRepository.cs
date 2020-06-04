352e9a9dc8f543a92b2ca06f027d65375276eaf3
unknown <guniv12@gmail.com>
2020-06-04 10:18:09 +0300

    Added concrete repository for testing

---
+++ MilitaryIndustrialComplexSolution/DALTests/TestAdminRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DALTests
+{
+    public class TestAdminRepository : BaseRepository<Admin>
+    {
+        public TestAdminRepository(DbContext context)
+            :base(context)
+        {
+
+        }
+    }
+}
