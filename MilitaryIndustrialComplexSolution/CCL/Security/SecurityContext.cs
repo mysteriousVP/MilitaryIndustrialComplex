385f7f17ed054d21b373376fb361afe79456733b
unknown <guniv12@gmail.com>
2020-06-05 09:53:59 +0300

    Realized security context
    Realized security context

---
+++ MilitaryIndustrialComplexSolution/CCL/Security/SecurityContext.cs
@@ -0,0 +1,24 @@
+﻿using CCL.Security.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security
+{
+    public static class SecurityContext
+    {
+        static User User = null;
+​
+        public static User GetUser()
+        {
+            return User;
+        }
+​
+        public static void SetUser(User user)
+        {
+            User = user;
+        }
+    }
+}
