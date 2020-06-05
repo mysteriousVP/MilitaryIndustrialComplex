02e73b9cbc3ea642544ee8786a4f6b8f8f3fd505
unknown <guniv12@gmail.com>
2020-06-05 09:53:28 +0300

    Added actors classes
    Added actors classes

---
+++ MilitaryIndustrialComplexSolution/CCL/Security/Identity/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security.Identity
+{
+    public class Admin
+        : User
+    {
+        public Admin(int userId, string name)
+            : base(userId, name, nameof(Admin))
+        {
+
+        }
+    }
+}
---
+++ MilitaryIndustrialComplexSolution/CCL/Security/Identity/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security.Identity
+{
+    public abstract class User
+    {
+        public int UserId { get; }
+        public string Name { get; }
+        protected string UserType { get; }
+
+        public User(int userId, string name, string userType)
+        {
+            UserId = userId;
+            Name = name;
+            UserType = userType;
+        }
+    }
+}
