eb1ef86dc8065d7a5a17082a33d8479c02ad8b7e
unknown <guniv12@gmail.com>
2020-06-06 12:30:32 +0300

    Facade tests updated
    Facade tests updated

---
+++ MilitaryIndustrialComplexSolution/ReportManagerTests/ReportFacadeTests.cs
@@ -0,0 +1,45 @@
+﻿using MilitaryIndustrialComplexSolution.ReportMakerFacade;
+using MilitaryIndustrialComplexSolution.ReportsManager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ReportManagerTests
+{
+    public class ReportFacadeTests
+    {
+        [Fact]
+        public void TestSuccesfullyExecutionOfCreatedFacade()
+        {
+            // arrange 
+            string emailData = "Email data";
+            string jsonData = "Json data";
+            string pdfData = "Pdf data";
+
+            ReportFacade facade = new ReportFacade(new EmailReportSender(emailData),
+                new JsonReportSender(jsonData), new PdfReportSender(pdfData));
+            ReportClient client = new ReportClient();
+
+            // act
+            // assert
+            Assert.True(client.CreateReports(facade));
+        }
+
+        [Fact]
+        public void TestFacadeOnExceptionThrowing()
+        {
+            // arrange 
+            string emailData = "Email data";
+            string pdfData = "Pdf data";
+
+            ReportFacade facade = new ReportFacade(new EmailReportSender(emailData),
+                null, new PdfReportSender(pdfData));
+            ReportClient client = new ReportClient();
+
+            // act
+            // assert
+            Assert.Throws<NullReferenceException>(() => client.CreateReports(facade));
+        }
+    }
+}
