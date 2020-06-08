0f6d10b3429bad14e8e28f8805832177ce258e75
unknown <guniv12@gmail.com>
2020-06-08 13:30:13 +0300

    Added ServerMediator class
    Added ServerMediator class

---
+++ MilitaryIndustrialComplexSolution/MilitaryIndustrialComplexSolution/MediatorNotifing/Mediator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryIndustrialComplexSolution
+{
+    public abstract class Mediator
+    {
+        public abstract bool SendEmail(string msg, Receiver receiver);
+    }
+}
---
+++ MilitaryIndustrialComplexSolution/MilitaryIndustrialComplexSolution/MediatorNotifing/ServerMediator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryIndustrialComplexSolution.MediatorNotifing
+{
+    public class ServerMediator : Mediator
+    {
+        public Receiver Production { get; set; }
+        public Receiver ProductionStore { get; set; }
+
+        public override bool SendEmail(string msg, Receiver receiver)
+        {
+            if (Production == receiver)
+            {
+                ProductionStore.Notify(msg);
+                return true;
+            }
+
+            if (ProductionStore == receiver)
+            {
+                Production.Notify(msg);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
