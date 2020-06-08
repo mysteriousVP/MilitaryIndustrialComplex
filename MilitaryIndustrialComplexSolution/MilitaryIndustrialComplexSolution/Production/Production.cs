3bb8c31b831de9874e6e7edfe0926555d4f6c56d
unknown <guniv12@gmail.com>
2020-06-08 12:33:33 +0300

    Added abstract Production class
    Added abstract Production class

---
+++ MilitaryIndustrialComplexSolution/MilitaryIndustrialComplexSolution/Production/Production.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryIndustrialComplexSolution.Production
+{
+    internal abstract class Production
+    {
+        public Production()
+        {
+            GetWeaponList();
+            ModifyWeaponList();
+            SaveChanges();
+            MakeLog();
+        }
+
+        public abstract void GetWeaponList();
+        public abstract void ModifyWeaponList();
+        public abstract void SaveChanges();
+        public virtual void MakeLog()
+        {
+            Console.WriteLine("Weapons list change was successful.\n");
+        }
+    }
+}
