f0d436d492ea3dcc1d89e8f2231ef2ee4ce23624
unknown <guniv12@gmail.com>
2020-06-08 12:50:41 +0300

    Added template tests
    Added template tests

---
+++ MilitaryIndustrialComplexSolution/ReportManagerTests/TemplateTests.cs
@@ -0,0 +1,33 @@
+﻿using MilitaryIndustrialComplexSolution.Production;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ReportManagerTests
+{
+    public class TemplateTests
+    {
+        [Fact]
+        public void TemplateRocketProductionWork()
+        {
+            // arrange
+            RocketProduction rocketProduction = new RocketProduction();
+
+            // act
+            // assert
+            rocketProduction.ModerateProduction();
+        }
+
+        [Fact]
+        public void TemplateWeaponProductionWork()
+        {
+            // arrange
+            WeaponProduction weaponProduction = new WeaponProduction();
+
+            // act
+            // assert
+            weaponProduction.ModerateProduction();
+        }
+    }
+}
